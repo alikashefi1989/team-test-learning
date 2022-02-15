d7f4cbdcc066fd8c29e19b8d5fb165ef9bd18473
Hamidreza Kazemi <hkazemi@travix.com>
2022-02-15 20:40:57 +0100

    tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import './App.css';
+// import './App.css';
 import Login from './components/login/Login';
 import Users from './components/user/users';
 
@@ -13,10 +13,9 @@ function App() {
 
   return (
     <>
-      <div className="App">
-        Gholiiiiiiiiiiii
-      </div>
-      <span>jafar3</span>
+      <h3 className="text-3xl font-bold underline mb-5 sm:text-green-400 md:text-green-700 2xl:text-red-500">react app testin</h3>
+      <h3 className="transition-all duration-1000 text-green-700 hover:text-green-300 mb-4 p-5 hover:p-14 bg-gray-300">transition</h3>
+
       <p style={{ textAlign: 'center' }}>{state}</p>
       <button onClick={gholi}>click me</button>
       <Users />
